d6d22cca1b55c202550e14b39f5c318cdd700615
EfeKiziltas <efe.kiziltas@saltpay.co>
2025-12-08 16:30:01 +0100

    add D5pt2

--- D5/2.php
+++ D5/2.php
@@ -1,32 +1,34 @@
 <?php
 
-$input = require 'input.php';
-$ranges = $input['ranges_demo'];
+$input = require "input.php";
+$ranges = $input["ranges_demo"];
 $count = 0;
+$count_diff = 0;
 
-$init = function () use (&$ranges, &$count) {
-	$len = count($ranges);
+$init = function () use ($count_diff, $ranges, &$count) {
+  $len = count($ranges);
 
-	for ($i = 0; $i < $len; $i++) {
-		preg_match('/(\d+)-(\d+)/', $ranges[$i], $matches);
-		$ranges[$i] = [
-			'start' => intval($matches[1]),
-			'end' => intval($matches[2])
-		];
-	}
+  for ($i = 0; $i < $len; $i++) {
+    preg_match("/(\d+)-(\d+)/", $ranges[$i], $matches);
+    $ranges[$i] = [
+      "start" => intval($matches[1]),
+      "end" => intval($matches[2]),
+    ];
+  }
 
-	usort($ranges, fn($a, $b) => $a['start'] <=> $b['start']);
+  usort($ranges, fn($a, $b) => $a["start"] <=> $b["start"]);
 
-	for ($i = 0; $i < $len; $i++) {
-		$start = $ranges[$i]['start'];
-		$end = $ranges[$i]['end'];
-		$prev_end = $i - 1 >= 0 ? $ranges[$i - 1]['end'] : 0;
-		$next_start = $i + 1 < $len ? $ranges[$i + 1]['start'] : PHP_INT_MAX;
-		if ($start > $prev_end && $end < $next_start) {
-			var_dump($start, $end, $prev_end, $next_start);
-			$count += ($end - $start) + 1;
-		}
-	}
+  for ($i = 0; $i < $len; $i++) {
+    $end = $ranges[$i]["end"];
+    $next_start = $i + 1 < $len ? $ranges[$i + 1]["start"] : PHP_INT_MAX;
+    if ($end > $next_start) {
+      $count_diff = $next_start - $end - 1;
+    }
+  }
+
+  $start = $ranges[0]["start"];
+  $end = $ranges[$len - 1]["end"];
+  $count = $end - $start + $count_diff;
 };
 
 $init();
