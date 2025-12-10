856c54dbaf63dfe716f65e54b61f000d144706a3
EfeKiziltas <efe.kiziltas@saltpay.co>
2025-12-10 17:41:39 +0100

    add

--- D7/1.php
+++ D7/1.php
@@ -1,16 +1,19 @@
 <?php
 
 $inputs = require "input.php";
-$input = $inputs["input"];
-$input_demo = $inputs["input_demo"];
-
+$input = $inputs["input_demo"];
 $starting_pos = strpos($input[0], 'S');
+$beams = [$starting_pos + 1, $starting_pos - 1];
 
-for ($i = 2; $i < count($input); $i++) {
+for ($i = 4; $i < count($input); $i++) {
   if ($i % 2 !== 0) continue;
   $str = $input[$i];
   for ($k = 2; $k < strlen($str); $k++) {
-    
+    $char = $str[$k];
+    if ($char === '^' && array_find($beams, fn(int $idx) => $idx === $k)) {
+      $beams = array_diff($beams, [$k]);
+      array_push($beams, $k + 1, $k - 1);
+    }
   }
 }
-var_dump($starting_pos);
+var_dump($beams);
