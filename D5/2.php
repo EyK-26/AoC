16256b407cb2a63168cb68c6166a1ef3bbb9ad08
EyK-26 <efeyagizkiziltas@gmail.com>
2025-12-12 16:56:25 +0100

    add missing

--- D5/2.php
+++ D5/2.php
@@ -1,30 +1,36 @@
 <?php
 
 $input = require "input.php";
-$ranges = $input["ranges_demo"];
+$ranges = $input["ranges"];
 $count = 0;
+$arr = [];
 
-$init = function () use (&$ranges, &$count) {
+$init = function () use (&$ranges, &$count, &$arr) {
 	$len = count($ranges);
 
 	for ($i = 0; $i < $len; $i++) {
 		preg_match("/(\d+)-(\d+)/", $ranges[$i], $matches);
 		$ranges[$i] = [
-			"start" => $matches[1],
-			"end" => $matches[2],
+			"start" => (int)$matches[1],
+			"end" => (int)$matches[2],
 		];
 	}
 
 	usort($ranges, fn($a, $b) => $a["start"] <=> $b["start"]);
+	$arr = [$ranges[0]];
 
-	for ($i = 0; $i < $len; $i++) {
-		$start = $ranges[$i]["start"];
-		$end = $ranges[$i]["end"];
-		$count += $end - $start + 1;
-		$next_start = $i + 1 < $len ? $ranges[$i + 1]["start"] : PHP_INT_MAX;
-		if ($end > $next_start) {
-			$count += $next_start - $end - 1;
+	for ($i = 1; $i < $len; $i++) {
+		$last = &$arr[count($arr) - 1];
+		$curr = $ranges[$i];
+		if ($last["end"] > $curr["start"]) {
+			$last["end"] = max($last["end"], $curr["end"]);
+			continue;
 		}
+		array_push($arr, $curr);
+	}
+
+	for ($i = 0; $i < count($arr); $i++) {
+		$count += ($arr[$i]["end"] - $arr[$i]["start"]) + 1;
 	}
 };
 
